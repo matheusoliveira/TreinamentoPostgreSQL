adf471b271aa7acbd4b15fa1bd8bc7d7287611f5
Matheus de Oliveira <matheus.oliveira@dextra.com.br>
2014-06-06 15:30:04 -0300

    IP/rede fixo em pacemaker_configure.sh

--- arquivos/pacemaker_configure.sh
+++ arquivos/pacemaker_configure.sh
@@ -1,22 +1,7 @@
 #!/bin/sh
 
-# INICIO EDIÇÃO:
-
-# Interface de rede a associar ao VIP
-iface=eth0
-# Último octeto do VIP (p.e. se IP da máquina for 192.168.1.X e master_sub_ip=200 então master_vip=192.168.1.200)
-master_sub_ip=200
-
-# FIM EDIÇÃO
-
-
-basedir=$(dirname $0)
-# Recupera IP e máscara
-inet_addr=$(ip -oneline -family inet addr show ${iface} | head -n1 | awk '{print $4}')
-ip=$(echo ${inet_addr} | awk -F '/' '{print $1}')
-netmask=$(echo ${inet_addr} | awk -F '/' '{print $2}')
-master_ip=$(echo ${ip} | awk -F '.' '{printf("%d.%d.%d.'${master_sub_ip}'", $1, $2, $3)}')
-bind_addr=$(echo ${ip} | awk -F '.' '{printf("%d.%d.%d.0", $1, $2, $3)}')
+master_ip=192.168.56.200
+netmask=24
 
 set -x
 
@@ -41,7 +26,8 @@ crm configure primitive pgsql ocf:heartbeat:pgsr \
     pgctl="/usr/local/pgsql/bin/pg_ctl" \
     psql="/usr/local/pgsql/bin/psql" \
     pgdata="/usr/local/pgsql/data" \
-    pgdba="postgres" pgport="5432"
+    pgdba="postgres" pgport="5432" \
+    op monitor interval="30s"
 
 # Força ambos a estarem juntos
 crm configure colocation pgsql-with-dbip inf: DBIP pgsql
